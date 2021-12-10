b32913663f4c1a6e352cce3bedc79aa287c16833
Simone Mattioli <simo.mattioli1998@gmail.com>
2021-12-10 12:23:49 +0100

    add use service view

--- parking_detector/detection_app/urls.py
+++ parking_detector/detection_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import acquire_detections, use_service
 
 urlpatterns = [
-    
+    path('', acquire_detections),
+    path('use_service/', use_service)
+
 ]
