aca1bbce5fb08099d716a526bcec7e8a107a2d61
Simone Mattioli <simo.mattioli1998@gmail.com>
2021-12-15 11:20:54 +0100

    generate model when the server starts

--- parking_detector/detection_app/apps.py
+++ parking_detector/detection_app/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from model.yolo import YoloModel
 
 class DetectionAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'detection_app'
+    VERSION = "v4"
+    SIZE = "full"
+    FOLDER = "model/building_files"
+    detector = YoloModel(VERSION, SIZE, FOLDER)
+    detector.build_model()
