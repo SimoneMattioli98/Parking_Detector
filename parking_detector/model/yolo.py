002ad84ae2e3bd732c401e91a2c93dec002d03f5
Simone Mattioli <simo.mattioli1998@gmail.com>
2022-01-13 19:15:46 +0100

    update yolo model with yolox

--- parking_detector/model/yolo.py
+++ parking_detector/model/yolo.py
@@ -1,11 +1,15 @@
 import torch 
 import numpy as np
+import os 
 
+from yolox.exp import get_exp
+from .predictor import Predictor
+from yolox.data.datasets import COCO_CLASSES
 
 class YoloModel:
     def __init__(self, version, folder) -> None:
         self.version = version
-        self.model = None
+        self.predictor = None
         self.folder = folder
         #classes which wants to be detected
         self.CLASSES_TO_DETECT = {
@@ -15,17 +19,59 @@ class YoloModel:
         }
 
 
+    def __create_predictor__(self, exp):
+        conf = 0.2
+        nms = 0.45
+        tsize = 1280
+        device = "gpu"
+        fp16 = False
+        ckpt = "../drive/MyDrive/parking-detector/Models/yolox_s.pth"
+        legacy = False
+    
+        exp.test_conf = conf
+        exp.nmsthre = nms
+        exp.test_size = (tsize, tsize)
+
+        model = exp.get_model()
+
+        if device == "gpu":
+            model.cuda()
+            if fp16:
+                model.half()  # to FP16
+        model.eval()
+
+        ckpt_file = ckpt
+        ckpt = torch.load(ckpt_file, map_location="cpu")
+        # load the model state dict
+        model.load_state_dict(ckpt["model"])
+
+        
+
+        predictor = Predictor(
+            model, exp, COCO_CLASSES, None, None,
+            device, fp16, legacy,
+        )
+    
+        return predictor 
+
+
     def build_model(self):
+        exp = get_exp(None, "yolox-s")
+        self.predictor = self.__create_predictor__(exp)
         # CHANGE MODEL TYPE BASED ON VERSION AND SIZE
-        self.model = torch.hub.load(self.folder, self.version)
         print("Model Built!")
 
+    def __predict__(self, image):
+        outputs, img_info = self.predictor.inference(image)
+        result_image = self.predictor.visual(outputs[0], img_info, self.predictor.confthre)
+        #cv2_imshow(result_image)
+        return outputs, result_image, img_info
+
     def get_detection(self, image):
-        if self.model == None:
+        if self.predictor == None:
             print("First you need to build the model")
         else:
-            #coordinates a retriven as x1,y1,x2,y2
-            results = self.model(image).xyxy[0]
-            mask = np.isin(results[:,5],list(self.CLASSES_TO_DETECT.keys()))
-            detections = results[mask]
-            return detections
+            outputs, result_image, img_info = self.__predict__(image)
+            # mask = np.isin(results[:,5],list(self.CLASSES_TO_DETECT.keys()))
+            # detections = results[mask]
+            return None
