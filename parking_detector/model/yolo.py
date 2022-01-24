b8dd68368d3a8fe750293c2e12cb5afa2b3fd4cd
Simone Mattioli <simo.mattioli1998@gmail.com>
2022-01-24 14:21:09 +0100

    fix prediction format

--- parking_detector/model/yolo.py
+++ parking_detector/model/yolo.py
@@ -72,7 +72,7 @@ class YoloModel:
             print("First you need to build the model")
         else:
             outputs, result_image, img_info = self.__predict__(image)
-            print(outputs)
+            print(type(list(self.CLASSES_TO_DETECT.keys())))
             mask = np.isin(outputs[:,6],list(self.CLASSES_TO_DETECT.keys()))
             detections = outputs[mask]
             return detections
